407f3d5444a8ca30c1a6476a7ff0d064259cdfb1
Jacob Lilly <106789970+JLilly78@users.noreply.github.com>
2025-04-19 14:44:17 -0400

    Co-authored-by: Daniel Lopez <djlopez5@users.noreply.github.com>

---
+++ prompts.md
@@ -0,0 +1,21 @@
+List of prompts:
+1. What classes do you recommend i take this semester
+2. When should I take CSCN 375?
+3. What is the hardest class in the degree?
+4. Why won't my professional advisor answer my emails?
+5. Help (put a message that says how to use this bot)
+6. Show me a schedule that only contains classes before 2pm
+7. Can you help me balance an easy and hard class load this semester?
+8. What classes still have open seats right now?
+9. Am I on track to graduate currently?
+10. What minor should I add based on my major and interests?
+ 
+ 
+ 
+
+depends what we are trying to achieve with each prompt. im assuming that with every new prompt we are showing a new dynamic to the ai to really flus
+tuo 
+
+
+
+uh oh are you lagging
--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+import jsonData from './response.json' with {type: "json"};
+
+
 var bot_text = document.getElementById("convotext");
 var text_box_inner = document.getElementById("textboxinner");
+
+console.log(jsonData);
 window.onload = function start(){
     bot_text = document.getElementById("convotext");
     text_box_inner = document.getElementById("textboxinner");
@@ -12,10 +17,13 @@ function changetext(chosenQuery) { //change the parameter to a string. make that
         bot_text.textContent = "OK, here is a calendar showing the classes you've chosen \n (This is where you would see a calendar, if I had one)";
     }
     else if (chosenQuery == 2) {
-        bot_text.textContent = "Based on what I can see from your previously taken courses, it seems like you can take CSCN 375, CSCN 342, BUSI 427, and others."
+        bot_text.textContent = "Based on what I can see from your previously taken courses, it seems like you can take CSCN 375, CSCN 342, BUSI 427, and others.";
     }
     else if (chosenQuery == 3) {
-        bot_text.textContent = "The ideal order of courses to take for the next 3 semesters is (This is where I would say that, but I don't feel like it today, sorry buddy)"
+        bot_text.textContent = "The ideal order of courses to take for the next 3 semesters is (This is where I would say that, but I don't feel like it today, sorry buddy)";
+    }
+    else if (chosenQuery == 4) {
+        bot_text.textContent = "what's up";
     }
 
 }
@@ -32,9 +40,13 @@ text_box_inner.addEventListener("keypress", e => {
         else if (input.includes("ideal order")) {
             changetext(3);
         }
+        else if (input.includes("hi")) {
+            changetext(4);
+        }
         else {
             bot_text.textContent = "What???";
         }
+        text_box_inner.value= "";
     }
 });
 
